c7046d1d0adbef8a42725df424ba61de9175e747
Yukky0103 <Yukky0103@users.noreply.github.com>
2024-10-22 10:40:37 +0900

    スロットサンプルイメージ導入

--- src/blacknip/RSM.java
+++ src/blacknip/RSM.java
@@ -3,6 +3,18 @@ package blacknip;
 import javax.swing.ImageIcon;
 
 public class RSM {
+	static ImageIcon[] slotImage;
+	
 	static ImageIcon normalButton = new ImageIcon(RSM.class.getClassLoader().getResource("赤ボタン.jpg"));
 	//static ImageIcon lampButton = new ImageIcon(RSM.class.getClassLoader().getResource("赤点灯ボタン.png"));
+	
+	public RSM(){
+		slotImage = new ImageIcon[16];
+	}
+	
+	public void prepareSlotImage() {
+		for(int i=0; i < slotImage.length; i++) {
+			slotImage[i] = new ImageIcon(RSM.class.getClassLoader().getResource(i + ".png"));
+		}
+	}
 }
